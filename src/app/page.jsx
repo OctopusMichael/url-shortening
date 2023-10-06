bf0b7a5bd45f31df133f439b18b040a0c8ed363d
Michael Diaz <diazcampomichael@gmail.com>
2023-10-05 20:26:29 -0500

    fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,6 @@ import ContentTwo from "@/components/ContentTwo";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
